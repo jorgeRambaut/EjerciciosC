85ac7307c92403420b08323edaa8aba33a8bed49
Jorge <jgrambaut@gmail.com>
2020-07-07 14:28:01 -0300

    agregado header para struct

---
+++ Proyecto/producto.h
@@ -0,0 +1,8 @@
+struct producto {
+char descripcion[13];// no se si esta bien tomar el tamaño en bytes
+char codigo[6];
+float cantidad;
+float precio;
+struct producto *ptrproducto;//ver cuando lo podemos usar 
+};
+
